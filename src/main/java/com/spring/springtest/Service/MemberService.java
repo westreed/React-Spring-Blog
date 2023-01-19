38e18c3fb16f59016c0b626615f05d1d6fcb531a
JangSeHun <westreed@naver.com>
2023-01-19 14:19:56 +0900

    CreateMemberService

---
+++ src/main/java/com/spring/springtest/Service/MemberService.java
@@ -0,0 +1,47 @@
+package com.spring.springtest.Service;
+
+import com.spring.springtest.domain.Member;
+import com.spring.springtest.repository.MemberRepository;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+            .ifPresent(m -> {
+                throw new IllegalStateException("이미 존재하는 이름입니다.");
+            });
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 ID 회원 조회
+     */
+    public Optional<Member> findOne(Long id){
+        return memberRepository.findById(id);
+    }
+}
