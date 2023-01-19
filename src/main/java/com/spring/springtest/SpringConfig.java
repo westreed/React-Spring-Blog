9b63f884a72a0088c8a75db03a7c6cfe31e776a5
JangSeHun <westreed@naver.com>
2023-01-19 21:56:35 +0900

    ADD: Spring Bean

---
+++ src/main/java/com/spring/springtest/SpringConfig.java
@@ -0,0 +1,20 @@
+package com.spring.springtest;
+
+import com.spring.springtest.Service.MemberService;
+import com.spring.springtest.repository.MemberRepository;
+import com.spring.springtest.repository.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+}
