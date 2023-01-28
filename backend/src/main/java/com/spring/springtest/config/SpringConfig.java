4c1c267bf2d37e3dc09deef429f3bc3b651c4cca
JangSeHun <westreed@naver.com>
2023-01-29 00:24:18 +0900

    Change: Folder Structure

---
+++ backend/src/main/java/com/spring/springtest/config/SpringConfig.java
@@ -0,0 +1,41 @@
+package com.spring.springtest.config;
+
+import com.spring.springtest.user.service.UserService;
+import com.spring.springtest.user.repository.JpaUserRepository;
+import com.spring.springtest.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.persistence.EntityManager;
+
+@Configuration
+public class SpringConfig {
+
+    private final EntityManager entityManager;
+
+    @Autowired
+    public SpringConfig(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Bean
+    public UserRepository userRepository(){
+        return new JpaUserRepository(entityManager);
+    }
+
+    @Bean
+    public UserService userService(){
+        return new UserService(userRepository());
+    }
+
+    //    @Bean
+//    public MemberService memberService(){
+//        return new MemberService(memberRepository());
+//    }
+//
+//    @Bean
+//    public MemberRepository memberRepository(){
+//        return new MemoryMemberRepository();
+//    }
+}
