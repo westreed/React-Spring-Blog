519c1c0be0c4976ba6eae9bf0ee258f6e942539c
JangSeHun <westreed@naver.com>
2023-02-19 23:05:28 +0900

    ADD: Add dev & prod environment

--- backend/src/main/java/com/spring/springserver/SpringServerApplication.java
+++ backend/src/main/java/com/spring/springserver/SpringServerApplication.java
@@ -1,13 +1,15 @@
 package com.spring.springserver;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-import java.util.TimeZone;
-
 @SpringBootApplication
 public class SpringServerApplication {
+
+	public SpringServerApplication(@Value("${variable.url}") String url) {
+		System.out.println("환경변수 설정 : " + url);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServerApplication.class, args);
