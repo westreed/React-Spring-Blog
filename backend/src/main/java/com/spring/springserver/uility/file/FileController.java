fdf4929badc78e7ce8fb2f108e2f49991b5f7fdb
JangSeHun <westreed@naver.com>
2023-02-19 23:06:41 +0900

    TODO: Upload and load images

--- backend/src/main/java/com/spring/springserver/uility/file/FileController.java
+++ backend/src/main/java/com/spring/springserver/uility/file/FileController.java
@@ -1,7 +1,9 @@
 package com.spring.springserver.uility.file;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,19 +25,27 @@ public class FileController {
     }
 
     @PostMapping("/api/upload")
-    public String uploadImage(@RequestParam("image") MultipartFile file){
+    public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file, @Value("${variable.url}") String envUrl){
+        // TODO: https://gilssang97.tistory.com/43 이미지 업로드 및 불러오기 만들기
         try {
             // 이미지 저장
             String fileName = file.getOriginalFilename();
             byte[] bytes = file.getBytes();
+            // 폴더가 없으면 생성해주기
+            Path uploadPath = Paths.get("uploads");
+            if (!Files.exists(uploadPath)) {
+                Files.createDirectory(uploadPath);
+            }
             Path path = Paths.get("uploads/" + fileName);
             Files.write(path, bytes);
 
+            String url = envUrl + "/uploads/" + fileName;
+            System.out.println(url);
             // 이미지 URL 반환
-            String url = "http://localhost:8080/uploads/" + fileName;
-            return url;
+            return ResponseEntity.ok(url);
         } catch (IOException e) {
-            throw new ResponseStatusException(HttpStatus.CONFLICT, e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+//            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
         }
     }
 }
