d5211facf2f1c18659723f03b65a6f990db0cf34
JangSeHun <westreed@naver.com>
2023-02-27 19:09:20 +0900

    UPDATE: different height from widget

--- frontend/src/pages/post.jsx
+++ frontend/src/pages/post.jsx
@@ -3,7 +3,7 @@ import Posts from "../components/posts";
 
 const Post = () => {
     return (
-        <div className="blogCard shadow-sm bg-body rounded" style={{width:"100%"}}>
+        <div className="blogCard shadow-sm bg-body rounded" style={{width:"100%", height:"100%"}}>
             <Posts/>
         </div>
     );
--- frontend/src/pages/settings.jsx
+++ frontend/src/pages/settings.jsx
@@ -4,7 +4,7 @@ import EditCategory from "../components/editCategory";
 
 const Settings = () => {
     return (
-        <div className="blogCard shadow-sm bg-body rounded" style={{width:"100%"}}>
+        <div className="blogCard shadow-sm bg-body rounded" style={{width:"100%", height:"100%"}}>
             <EditCategory/>
         </div>
     );
